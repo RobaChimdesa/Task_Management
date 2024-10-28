9288d6d80f2ec66844c5cbd730a629183494b09b
RobaChimdesa <robachimdesa123@gmail.com>
2024-10-28 11:10:48 +0300

    commit

--- Backend/TaskManage/UserTask/admin.py
+++ Backend/TaskManage/UserTask/admin.py
@@ -3,5 +3,17 @@ from .models import Task,Catagory
 # Register your models here.
 
 
-# admin.site.register(Task)
-# admin.site.register(Catagory)
+# admin.py
+from django.contrib import admin
+from .models import Task,Catagory
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'catagory', 'user', 'deadline', 'completed')
+    list_filter = ('completed', 'catagory', 'user')
+    search_fields = ('title', 'description ')
+
+@admin.register(Catagory)
+class CatagoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    search_fields = ('name',)
--- Backend/TaskManage/UserTask/urls.py
+++ Backend/TaskManage/UserTask/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet,CategoryViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 
@@ -8,4 +12,7 @@ router.register(r'tasks',TaskViewSet,basename='task')
 router.register(r'categories',CategoryViewSet,basename='category')
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
